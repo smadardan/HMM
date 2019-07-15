26a921d4300e4d880455d30fd7a722fd59e45d5a
Smadar Danon <smadardanon2@gmail.com>
2019-07-15 15:44:39 +0300

    added license, requirements and setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='HMM',
+      version='1.0',
+      description='Python Distribution Utilities',
+      author='Smadar Danon',
+      author_email='smadardanon2@gmail.com',
+      packages=['numpy', 'pytest'],
+      license='MIT',
+      python_requires='>=3.6.0'
+     )
